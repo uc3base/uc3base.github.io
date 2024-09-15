98275ed82bc9dea3a703ededc926dd0c20f1dc41
Juan María <jmherreramartin@gmail.com>
2024-09-15 03:09:10 +0300

    Draft 1 esqueleto básico

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Ejemplo de validación de formulario
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Formulario enviado. Gracias por contactarnos.');
+});
