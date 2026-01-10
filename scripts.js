d61499d7dce2f93c9ffb3b3fc9deee222e849f41
Alfooon <139629154+Alfonso-masterAccount@users.noreply.github.com>
2026-01-10 01:33:35 +0100

    Web Versión 1.0

--- scripts.js
+++
@@ -1,5 +0,0 @@
-// Ejemplo de validación de formulario
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Formulario enviado. Gracias por contactarnos.');
-});
